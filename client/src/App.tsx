a08ded3c11e814a86afc4d56521398254b720491
devinfinitee <vickeysmatt@gmail.com>
2025-11-16 18:04:11 +0000

    fixed the link issues

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,17 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { ThemeProvider } from "@/components/ThemeProvider";
-import { CurrencyProvider } from "@/components/CurrencyProvider";
-import NotFound from "@/pages/not-found";
-import Home from "@/pages/Home";
-import Services from "@/pages/Services";
-import Courses from "@/pages/Courses";
-import Contact from "@/pages/Contact";
-import Login from "@/pages/Login";
-import Register from "@/pages/Register";
+import { Toaster } from "./components/ui/toaster";
+import { TooltipProvider } from "./components/ui/tooltip";
+import { ThemeProvider } from "./components/ThemeProvider";
+import { CurrencyProvider } from "./components/CurrencyProvider";
+import NotFound from "./pages/not-found";
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import Courses from "./pages/Courses";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function Router() {
   return (
